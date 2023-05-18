41cc0235c5949d4c9f08c4e2484d523651091e1f
Jorge Sanchez <themeandata@outlook.com>
2023-05-17 22:44:05 -0400

    transformers, baby!

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 import streamlit as st
-import pandas as pd
+from transformers import pipeline
+
+# Initialize the translation pipeline
+translator = pipeline("translation_en_to_es", model="Helsinki-NLP/opus-mt-en-es")
 
 # Set the app title
-st.title("Jorge new change- My Streamlit App")
+st.title("Jorge - My Streamlit App")
 
 # Add a text input field
-name = st.text_input("Enter your name")
+#name = st.text_input("Enter your name")
 
 # Display a greeting
-st.write(f"Hello, {name}!")
+#st.write(f"Hello, {name}!")
 
-# Create a simple pandas DataFrame
-df = pd.DataFrame({
-    'first column': list(range(1, 5)),
-    'second column': list(range(5, 9))
-})
+# Add a text input field for the text to translate
+text_to_translate = st.text_input("Enter some text to translate to Spanish")
 
-# Display the DataFrame in the app
-st.write(df)
+# Only translate if there is text to translate
+if text_to_translate:
+    # Use the translator to translate the text
+    translation = translator(text_to_translate)[0]['translation_text']
+
+    # Display the translated text
+    st.write(translation)
