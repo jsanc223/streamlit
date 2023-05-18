603507c1ac7d064d4b9bca653042b42ecf3e137f
Jorge Sanchez <themeandata@outlook.com>
2023-05-17 22:22:08 -0400

    Added pandas to app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 # Set the app title
 st.title("Jorge - My Streamlit App")
@@ -9,3 +10,11 @@ name = st.text_input("Enter your name")
 # Display a greeting
 st.write(f"Hello, {name}!")
 
+# Create a simple pandas DataFrame
+df = pd.DataFrame({
+    'first column': list(range(1, 5)),
+    'second column': list(range(5, 9))
+})
+
+# Display the DataFrame in the app
+st.write(df)
